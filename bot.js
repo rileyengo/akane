6e4d7115264eb59b6470618b1011a55e21116ad5
Americhino <rileyengo@gmail.com>
2018-08-29 17:39:30 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,15 +13,23 @@ client.on('ready', () => {
  
 
 client.on('message', message => {
-
+				var randomLoveYourself = 'They call me artist, but they can\'t all me idol :)', 'It\'s in our DNA ;)', 'fAKE loVE';
     if (message.content === '!ping') {
         message.reply('pong');
     }
-
+    if (message.content === '!shampoo') {
+        message.reply('我爱你！ Wo ai ni!');
+    }
+    if (message.content === '!loveyourself') {
+        message.reply(randomLoveYourself);
+    }
+    if (['shampoo', 'ukyo', 'kodachi', 'kuno'].indexOf(message.content) === -1) {
+        message.reply('Hmph.'); 
+    }
 });
 
  
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN); // sorry to disappoint y'all
