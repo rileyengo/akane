4b48e4d868b8be0ac6472a657a91dd9be18e7029
Americhino <rileyengo@gmail.com>
2018-08-30 16:02:31 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,116 +13,67 @@ client.on('ready', () => {
  
 
 client.on('message', message => {
-    if (message.content === '!ping') {
+	const prefix = '!';
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+    if (command === 'ping') {
         message.reply('pong');
     }
-    if (message.content === '!shampoo') {
+    if (command === 'shampoo') {
         message.reply('我爱你! Wo ai ni!');
     }
-    if (message.content === '!loveyourself') {
-        message.reply('You can\'t stop me loving myself!');
+    if (command === 'loveyourself') {
+		const loveYourself = ['You can\'t stop me loving myself!', 'It\'s all fAKE loVE', 'It\'s in our DNA.']
+        message.channel.send(loveYourself);
     }
     if (message.content === 'ayy') {
         message.channel.send('LMAOO'); 
     }
-	if (message.content === '!fruits') {
+	if (command === 'fruits') {
 		message.react('🍎');
 		message.react('🍊');
 		message.react('🍇');
 	}
-	if (message.content === '!hug') {
+	if (command === 'hug') {
     	message.reply('love you, bae! <3');
 		message.react('✨')
 		message.react('💖')
 		message.react('💕')
 	}
-	if (message.content === `!server`) {
+	if (command === `server`) {
     	message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 	}
-	if (message.content === `!user-info`) {
+	if (command === `user-info`) {
     	message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	}
- // If the message content starts with "!kick"
- if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member.kick('Optional reason that will display in the audit logs').then(() => {
-          // We let the message author know we were able to kick the person
-          message.reply(`Successfully kicked ${user.tag}`);
-        }).catch(err => {
-          // An error happened
-          // This is generally due to the bot not being able to kick the member,
-          // either due to missing permissions or role hierarchy
-          message.reply('I was unable to kick the member');
-          // Log the error
-          console.error(err);
-        });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply('that user isn\'t in this guild!');
-      }
-    // Otherwise, if no user was mentioned
-    } else {
-      message.reply('you didn\'t mention the user to kick!');
-    }
-  }
-   // if the message content starts with "!ban"
-  if (message.content.startsWith('!ban')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Ban the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         * Read more about what ban options there are over at
-         * https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=ban
-         */
-        member.ban({
-          reason: 'Misbehavior',
-        }).then(() => {
-          // We let the message author know we were able to ban the person
-          message.reply(`Successfully banned ${user.tag}`);
-        }).catch(err => {
-          // An error happened
-          // This is generally due to the bot not being able to ban the member,
-          // either due to missing permissions or role hierarchy
-          message.reply('I was unable to ban the member');
-          // Log the error
-          console.error(err);
-        });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply('That user isn\'t in this guild!');
-      }
-    } else {
-    // Otherwise, if no user was mentioned
-      message.reply('you didn\'t mention the user to ban!');
-    }
-  }
-  // If message is '!avatar'
-  if (message.content === '!avatar') {
-    // Send the user's avatar URL
-    message.reply(message.author.avatarURL);
-  } /*
+	if (command === "kick") {
+		let member = message.mentions.members.first();
+		let reason = args.slice(1).join(" ");
+		member.kick(reason);
+	}
+	if (command === "say"){
+		let text = args.slice(1).join(" ");
+		message.delete();
+		message.channel.send(text);
+	}
+	if (command === "addrole"){
+		let role = message.guild.roles.find("name", "Team Mystic");
+		let member = message.mentions.members.first();
+		member.addRole(role).catch(console.error);
+	}
+	if (command === "removerole"){
+		let role = message.guild.roles.find("name", "Team Mystic");
+		let member = message.mentions.members.first();
+		member.removeRole(role).catch(console.error);
+	}
+	if (command === "ban") {
+		let member = message.mentions.members.first();
+		let reason = args.slice(1).join(" ");
+		member.ban(reason);
+	}
+	if (command === 'avatar') {
+    	message.reply(message.author.avatarURL);
+  	} /*
   if (message.content.startsWith('!avatar ')) {
     // Send the user's avatar URL
     message.reply(message.author.avatarURL);
@@ -136,7 +87,7 @@ client.on('message', message => {
       }
   } */
 	// !info
-	if (message.content === '!info') {
+	if (command === 'info') {
 		message.channel.send({embed: {
 		color: 0xe24b4b,
 		author: {
@@ -160,6 +111,7 @@ client.on('message', message => {
 	});
 	}
 });
+
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN); // sorry to disappoint y'all
