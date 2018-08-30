d7169b7aade61e402078011bf275dcf14c0945f7
Americhino <rileyengo@gmail.com>
2018-08-30 16:24:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('message', message => {
 		  snekfetch = require('snekfetch'),
 		  querystring = require('querystring');
 	async function googleCommand(msg, args) {
-		let searchMessage = await <Message>.reply('Searching... Sec.');
+		let searchMessage = await message.reply('Searching... Sec.');
 		let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(msg.content)}`;
 		return snekfetch.get(searchUrl).then((result) => {
 			let $ = cheerio.load(result.text);
