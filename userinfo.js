4ab944920bfffbacaa219ba6691b44f9ac17278c
Americhino <rileyengo@gmail.com>
2018-09-01 12:35:35 -0600

    Update userinfo.js

--- userinfo.js
+++ userinfo.js
@@ -1,30 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('message', message => {
-	let member = message.mentions.members.first();
-	let embed = new Discord.RichEmbed();
-	/*********************************************/
-	module.exports.embed = embed
-	/*********************************************/
-		.setTitle(member.user.tag)
-		.setAuthor(member.user.username, member.user.displayAvatarURL)
-		.setColor(0x00AE86)
-		.setFooter(member.user.username, member.user.displayAvatarURL)
-		// .setImage("http://i.imgur.com/yVpymuV.png")
-		.setThumbnail(member.user.avatarURL)
-		.setTimestamp()
-		.addField("Nickname",
-		member.displayName)
-		.addField("User ID",
-		member.id)
-		.addField("Joined Guild",
-		member.joinedAt)
-		.addField("Created at",
-		member.user.createdAt)
-		.addField("Status",
-		member.presence.status)
-		.addField("Bot",
-		member.user.bot)
-		.addField("Presence",
-		member.user.presence.name)
+	if (command === 'user') {
+		let member = message.mentions.members.first();
+		let embed = new Discord.RichEmbed();
+			.setTitle(member.user.tag)
+			.setAuthor(member.user.username, member.user.displayAvatarURL)
+			.setColor(0x00AE86)
+			.setFooter(member.user.username, member.user.displayAvatarURL)
+			// .setImage("http://i.imgur.com/yVpymuV.png")
+			.setThumbnail(member.user.avatarURL)
+			.setTimestamp()
+			.addField("Nickname",
+			member.displayName)
+			.addField("User ID",
+			member.id)
+			.addField("Joined Guild",
+			member.joinedAt)
+			.addField("Created at",
+			member.user.createdAt)
+			.addField("Status",
+			member.presence.status)
+			.addField("Bot",
+			member.user.bot)
+			.addField("Presence",
+			member.user.presence.name)
+			message.channel.send({embed});
+	}
 });
