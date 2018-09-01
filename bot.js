180a672307795fac37b42f794f61839fbd1c8556
Americhino <rileyengo@gmail.com>
2018-09-01 11:34:21 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ client.on('message', message => {
   	}
 	if (command === 'trivia') {
 	const questions = require("./questions.js");
-	message.channel.send(questions.QSelect([/([^\D_]+[^\s-]*) */]));
+	message.channel.send(questions.QSelect());
     	message.reply('Trivia time! ' + questions.QText + '\n You have 15 seconds to answer.')
 			.then(() => {
 			message.channel.awaitMessages(response => response.content === questions.QAnswer, {
