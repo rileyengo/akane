081cd21cc3d3de3650ea976a6ede7a6c52859943
Americhino <rileyengo@gmail.com>
2018-09-01 17:01:12 -0600

    Update music.js

--- music.js
+++ music.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const music = require('discord.js-music-v11');
 const Bot = new Discord.Client();
-const token = "<auth_token>" // Recommended to load from json file.
+const token = "process.env.BOT_TOKEN" 
  
 Bot.on('ready', () => {
     console.log(`[Start] ${new Date()}`);
