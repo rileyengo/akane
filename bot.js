ab088f1edb1e1f6278e65d66cbfa2309654a3637
Americhino <rileyengo@gmail.com>
2018-08-31 20:17:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,12 +57,12 @@ client.on('message', message => {
 		message.channel.send(text);
 	}
 	if (command === "addrole"){
-		let role = message.guild.roles.find("name", "Team Mystic");
+		let role = message.guild.roles.find("name", "Music");
 		let member = message.mentions.members.first();
 		member.addRole(role).catch(console.error);
 	}
 	if (command === "removerole"){
-		let role = message.guild.roles.find("name", "Team Mystic");
+		let role = message.guild.roles.find("name", "Music");
 		let member = message.mentions.members.first();
 		member.removeRole(role).catch(console.error);
 	}
@@ -91,7 +91,43 @@ client.on('message', message => {
 				message.channel.send('Wrong! The correct answer was ' + questions.QAnswer + '.');
 			});
 		});
-  	} /*
+  	} 
+	if (command === 'google') {
+		// The modules we are using are cheerio, snekfetch, and querystring.
+const cheerio = require('cheerio'),
+      snekfetch = require('snekfetch'),
+      querystring = require('querystring');
+
+// Depending on your command framework (or if you use one), it doesn't have to
+// edit messages so you can rework it to fit your needs. Again, this doesn't have
+// to be async if you don't care about message editing.
+async function googleCommand(msg, args) {
+
+   // These are our two variables. One of them creates a message while we preform a search,
+   // the other generates a URL for our crawler.
+   let searchMessage = await <Message>.reply('Searching... Sec.');
+   let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(msg.content)}`;
+
+   // We will now use snekfetch to crawl Google.com. Snekfetch uses promises so we will
+   // utilize that for our try/catch block.
+   return snekfetch.get(searchUrl).then((result) => {
+
+      // Cheerio lets us parse the HTML on our google result to grab the URL.
+      let $ = cheerio.load(result.text);
+
+      // This is allowing us to grab the URL from within the instance of the page (HTML)
+      let googleData = $('.r').first().find('a').first().attr('href');
+
+      // Now that we have our data from Google, we can send it to the channel.
+      googleData = querystring.parse(googleData.replace('/url?', ''));
+      searchMessage.edit(`Result found!\n${googleData.q}`);
+
+  // If no results are found, we catch it and return 'No results are found!'
+  }).catch((err) => {
+     searchMessage.edit('No results found!');
+  });
+}
+	} /*
   if (message.content.startsWith('!avatar ')) {
     // Send the user's avatar URL
     message.reply(message.author.avatarURL);
