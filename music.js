9a82eff6b728e2a893d83ec6847bfd93b680364d
Americhino <rileyengo@gmail.com>
2018-09-01 15:05:03 -0600

    Update music.js

--- music.js
+++ music.js
@@ -94,4 +94,4 @@ client.on('message', msg => {
 	if (!msg.content.startsWith(prefix)) return;
 	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(prefix.length).split(' ')[0]](msg);
 });
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
