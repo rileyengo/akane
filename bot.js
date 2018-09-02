d4ba295e8f223aaa03ca1d3e941b7286f4080e62
Americhino <rileyengo@gmail.com>
2018-09-02 15:49:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,57 @@ client.on('message', message => {
 	} catch (err) {
 		console.error(err);
 	}
+	// Embeds need to be here for some reason 
+	if (command === 'server') {
+		let member = message.mentions.members.first();
+		const embed = new Discord.RichEmbed()
+			.setTitle(message.guild.name)
+			.setAuthor(message.guild.name, message.guild.iconURL)
+			.setColor(0x00AE86)
+			.setFooter(message.author.tag, message.author.displayAvatarURL)
+			// .setImage("http://i.imgur.com/yVpymuV.png")
+			.setThumbnail(message.guild.iconURL)
+			.setTimestamp()
+			.addField("Owner",
+				message.guild.owner.toString())
+			.addField("ID",
+				message.guild.id)
+			.addField("Region",
+				message.guild.region)
+			.addField("Created at",
+				message.guild.createdAt)
+			.addField("Channels",
+				message.guild.channels)
+			.addField("Users",
+				message.guild.memberCount)
+			.addField("Roles",
+				message.guild.roles);
+			message.channel.send({embed});
+	}
+	if (command === 'user') {
+		let member = message.mentions.members.first();
+		const embed = new Discord.RichEmbed()
+			.setTitle(member.user.tag)
+			.setAuthor(member.user.username, member.user.displayAvatarURL)
+			.setColor(0x00AE86)
+			.setFooter(message.author.tag, message.author.displayAvatarURL)
+			// .setImage("http://i.imgur.com/yVpymuV.png")
+			.setThumbnail(member.user.avatarURL)
+			.setTimestamp()
+			.addField("Nickname",
+				member.displayName)
+			.addField("User ID",
+				member.id)
+			.addField("Joined Guild",
+				member.joinedAt)
+			.addField("Created at",
+				member.user.createdAt)
+			.addField("Status",
+				member.presence.status)
+			.addField("Bot",
+				member.user.bot);
+			message.channel.send({embed});
+	}
 });
 // THIS  MUST  BE  THIS  WAY
 
