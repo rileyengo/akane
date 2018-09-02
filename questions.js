2b806517da938d83d00ff12204ea4f8ac1a56371
Americhino <rileyengo@gmail.com>
2018-09-01 20:21:30 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -33,6 +33,6 @@ function QSelect() {
 	"Russia": "Moscow",
 	"Ukraine": "Kiev",
 	}
-	module.exports.question = Questions[Math.floor(Math.random() * Math.floor(Questions))]
+	module.exports.question = Questions[Math.floor(Math.random() * Math.floor(Questions.length))]
 	module.exports.answer = Answers[module.exports.question]
 }
