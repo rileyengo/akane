cabdfd68baf1562b63897b9a314375a1ea289a84
Americhino <rileyengo@gmail.com>
2018-09-02 12:01:07 -0600

    Create fruits.js

---
+++ commands/fruits.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+const randomFruits = new Array("🍎", "🍊", "🍇", "🍑", "🍊", "🍓", "🍍", "🍉", "🍌", "🍒", "🍋", "🍏", "🍐",)
+const fruit = randomFruits[Math.floor(Math.random() * Math.floor(Math.floor(Math.floor(Math.floor(Math.floor(Math.floor(Math.floor(Math.floor(Math.floor(randomFruits.length))))))))))];
+	message.react(fruit);
+	message.react(fruit);
+	message.react(fruit);
+}
