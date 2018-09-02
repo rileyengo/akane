b865ef9496d254028ebab58d3a24c2b659f2ae6c
Americhino <rileyengo@gmail.com>
2018-09-02 17:13:06 -0600

    Update quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -1,14 +1,1 @@
- exports.run = (client, message, args) => {
-	const randomQuote = require("./data/quotes.js");
-	const embed = {
-  	'title': "**Random Quote**",
-  	'description': '```js\n\"' + randomQuote.quote + '```',
-  	'color': 5301186,
-  	'timestamp': '2018-09-02T22:48:28.432Z',
-  	'footer': {
-    	'icon_url': message.author.displayAvatarURL,
-    	'text': message.author.tag
-  	}
-};
-	message.channel.send(embed)
-}
+
