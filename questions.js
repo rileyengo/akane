4bd7ac808ad9fd08cf8ab5beaeab816e7dc3ad18
Americhino <rileyengo@gmail.com>
2018-09-01 20:10:51 -0600

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,36 @@
+module.exports.Questions = "World Capitals Trivia by Americhino"
+function QSelect() {
+	const Questions = new Array("Norway", "Hungary", "Cyprus", "Libya", "Kenya", "Nigeria", "Columbia", "Barbados", "Australia", "Pakistan", "Spain", "England", "Ireland", "Sweden", "Denmark", "Poland", "Italy", "India", "Canada", "Portugal", "South Korea", "Japan", "China", "Indonesia", "France", "Serbia",);
+	const Answers = {
+	"Norway": "Oslo",
+	"Hungary": "Budapest",
+	"Cyprus": "Nicosia",
+	"Libya": "Tripoli",
+	"Kenya": "Nairobi",
+	"Nigeria": "Lagos",
+	"Columbia": "Bogota",
+	"Barbados": "Bridgetown",
+	"Australia": "Canberra",
+	"Pakistan": "Islamabad",
+	"Spain": "Madrid", // No cheating!
+	"England" : "London",
+	"Ireland": "Dublin",
+	"Sweden": "Stockholm",
+	"Denmark": "Copenhagen",
+	"Poland": "Warsaw",
+	"Italy": "Rome",
+	"India": "New Delhi",
+	"Canada": "Ottawa",
+	"Portugal": "Lisbon",
+	"South Korea": "Seoul",
+	"Japan": "Tokyo",
+	"China": "Beijing",
+	"Indonesia": "Jakarta",
+	"France": "Paris",
+	"Serbia": "Belgrade",
+	"Russia": "Moscow",
+	"Ukraine": "Kiev",
+	}
+	module.exports.question = Questions[Math.floor(Math.random() * Questions.length)]
+	module.exports.answer = Answers[module.exports.question]
+}
