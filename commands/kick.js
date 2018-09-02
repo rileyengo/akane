05adc1eaebd4792d982d0bd7c5f09e48147899ab
Americhino <rileyengo@gmail.com>
2018-09-02 11:53:12 -0600

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,18 @@
+exports.run = (client, message, [mention, ...reason]) => {
+const modRole = message.guild.me.hasPermission("KICK_MEMBERS");
+	if (!modRole)
+	return console.log(You cannot kick users.")
+	return message.reply("You cannot kick users.");
+
+	if (message.mentions.members.size === 0)
+	return message.reply("Please mention a user to kick");
+
+	if (!message.guild.me.hasPermission("KICK_MEMBERS"))
+	return message.reply("");
+	
+	const kickMember = message.mentions.members.first();
+
+	kickMember.kick(reason.join(" ")).then(member => {
+	message.reply(`${member.user.username} was succesfully kicked.`);
+	});
+};
