ccfbc76feb5b4a8acda27e318740a40c0938c8bd
Americhino <rileyengo@gmail.com>
2020-06-09 19:59:02 -0600

    fancy embed testing uwu

--- commands/trivia.js
+++ commands/trivia.js
@@ -2,7 +2,15 @@ exports.run = (Discord, client, message, args) => {
 	const questions = require("./trivia/questions.js");
 	const question = questions.Questions[Math.floor(Math.random() * Math.floor(questions.Questions.length))];
 	const answer = questions.Answers[question]
-    message.channel.send('Trivia time! What is the capital of ' + question + '? \n You have 15 seconds to answer.')
+	let member = message.mentions.members.first();
+	const embed = new Discord.RichEmbed()
+		.setTitle(member.user.tag)
+		.setAuthor(member.user.username, member.user.displayAvatarURL)
+		.setColor(16272203)
+		.setFooter(member.user.username, member.user.displayAvatarURL)
+		.setTimestamp()
+		.setDescription('Trivia time! What is the capital of' + question + '? \n You have 15 seconds to answer.')
+    message.channel.send({embed})
 		.then(() => {
 		message.channel.awaitMessages(response => response.content === answer, {
 			max: 1,
@@ -15,5 +23,5 @@ exports.run = (Discord, client, message, args) => {
 		.catch(() => {
 			message.channel.send('❌ Wrong! The correct answer was ' + answer + '.');
 		});
-	});
+	}); 
 }
