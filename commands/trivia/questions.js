c0c4ebf8fdb325e9eb1121f8bd0d075dd1a7aa81
Americhino <rileyengo@gmail.com>
2020-06-12 11:05:43 -0600

    ASIA BOIIIIIIIIII HECC YE

--- commands/trivia/questions.js
+++ commands/trivia/questions.js
@@ -6,7 +6,7 @@ module.exports.Questions = "World Capitals Trivia by Americhino"
 	module.exports.QText   = QSelect(); // The questions
 	module.exports.QAnswer = module.exports.QText; // The correct answers
 function QSelect() {
-	module.exports.Questions = new Array("Norway", "Hungary", "Cyprus", "Libya", "Kenya", "Nigeria", "Columbia", "Barbados", "Australia", "Pakistan", "Spain", "United Kingdom", "Ireland", "Sweden", "Denmark", "Poland", "Italy", "India", "Canada", "Portugal", "South Korea", "Japan", "China", "Indonesia", "France", "Serbia", "Russia", "Ukraine", "United States",);
+	module.exports.Questions = new Array("Norway", "Hungary", "Cyprus", "Libya", "Kenya", "Nigeria", "Columbia", "Barbados", "Australia", "Pakistan", "Spain", "United Kingdom", "Ireland", "Sweden", "Denmark", "Poland", "Italy", "India", "Canada", "Portugal", "South Korea", "Japan", "China", "Indonesia", "France", "Serbia", "Russia", "Ukraine", "United States", "Thailand", "Philippines", "Afghanistan", "Armenia", "Azerbaijan", "Bahrain", "Bangladesh", "Bhutan", "Brunei", "Cambodia", "Georgia", "Iran", "Iraq", "Jordan", "Kazakhstan", "Kuwait", "Kyrgyzstan", "Laos", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Myanmar", "Nepal", "North Korea", "Oman", "Papua New Guinea", "Qatar", "Saudi Arabia", "Singapore", "Sri Lanka", "Syria", "Taiwan", "Tajikistan", "Turkey", "Turkmenistan", "United Arab Emirates (UAE)", "Uzbekistan", "Vietnam", "Yemen",);
 	module.exports.Answers = {
 	"Norway": "Oslo",
 	"Hungary": "Budapest",
@@ -37,5 +37,46 @@ function QSelect() {
 	"Russia": "Moscow",
 	"Ukraine": "Kiev",
 	"United States": "Washington, D.C.",
+	"Thailand": "Bangkok",
+	"Philippines": "Manila",
+	"Afghanistan": "Kabul",
+	"Armenia": "Yerevan",
+	"Azerbaijan": "Baku",
+	"Bangladesh": "Dhaka",
+	"Bhutan": "Thimpu",
+	"Brunei": "Bandar Seri Begawan",
+	"Cambodia": "Phnom Penh",
+	"East Timor": "Dili",
+	"Georgia": "Tbilisi",
+	"Iran": "Tehran",
+	"Iraq": "Baghdad",
+	"Israel": "Jerusalem",
+	"Jordan": "Amman",
+	"Kazakhstan": "Nur-Sultan",
+	"Kuwait": "Kuwait City",
+	"Kyrgyzstan": "Bishkek",
+	"Laos": "Vientiane",
+	"Lebanon": "Beirut",
+	"Malaysia": "Kuala Lumpur",
+	"Maldives": "Male",
+	"Mongolia": "Ulaanbataar",
+	"Myanmar": "Naypiydaw",
+	"Nepal": "Kathmandu",
+	"North Korea": "Pyongyang", // ahahhhah pog
+	"Oman": "Muscat",
+	"Papua New Guinea": "Port Moresby",
+	"Qatar": "Doha",
+	"Saudi Arabia": "Riyadh",
+	"Singapore": "Singapore",
+	"Sri Lanka": "Sri Jayawardenepura Kotte", // rip in pieces non-geography nerds and non-sri lankans
+	"Syria": "Damascus",
+	"Taiwan": "Taipei", // urg don't sue me pro-china people srry
+	"Tajikistan": "Dushanbe",
+	"Turkey": "Ankara",
+	"Turkmenistan": "Ashgabat",
+	"United Arab Emirates (UAE)": "Abu Dhabi",
+	"Uzbekistan": "Tashkent",
+	"Vietnam": "Hanoi",
+	"Yemen": "Sana'a",
 	}
 }
