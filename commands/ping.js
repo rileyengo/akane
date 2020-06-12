7eacf0c3486217d92e7f334c98a7e6e9d066fcba
Americhino <rileyengo@gmail.com>
2020-06-12 11:44:46 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,3 @@
 exports.run = (Discord, client, message, args) => {
-    message.channel.send("Ping?").then((sentMessage) => sentMessage.edit("Pong! `${Math.round(client.ping)}ms`"))
+    message.channel.send("Ping?").then((sentMessage) => sentMessage.edit("Pong! `" + Math.round(client.ping) + "ms`"))
 }
