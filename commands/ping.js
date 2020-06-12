e262252f2d481b43130475e1aafb22effd228f81
Americhino <rileyengo@gmail.com>
2020-06-12 11:36:09 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
 exports.run = (Discord, client, message, args) => {
-    const m = await message.channel.send("Ping?");
+    const m = message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`).catch(console.error);
 }
