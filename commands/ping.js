ddee3b45f5d42fb1dea61a015e0a82c836b4838a
Americhino <rileyengo@gmail.com>
2020-06-12 11:49:55 -0600

    urg

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,5 @@
 exports.run = (Discord, client, message, args) => {
-    message.channel.send("Ping?").then((sentMessage) => sentMessage.edit("Pong! `" + Math.round(client.ping) + "ms`"))
+    message.channel.send("Ping?").then(setTimeout(function(){ 
+        (sentMessage) => sentMessage.edit("Pong! `" + Math.round(client.ping) + "ms`");
+    }, 2000))
 }
