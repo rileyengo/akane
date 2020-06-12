d51f2e6a47be9ee613d909c998f27040e75d802e
Americhino <rileyengo@gmail.com>
2020-06-12 11:29:52 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,23 @@
-exports.run = (Discord, client, message, args) => {
-    message.channel.send("pong! `" + + Math.round(client.ws.ping) + "`").catch(console.error);
+exports.run = (Discord, client, message, args, command) => {
+    client.on('messageUpdate', (oldMessage, newMessage) => {
+
+        if (oldMessage.content === 'Pinging...') {
+    
+            var oldMessageTimestamp = oldMessage.createdTimestamp;
+            var newMessageTimestamp = newMessage.editedTimestamp;
+    
+            newMessage.edit(`🏓 Pong!\nPonged back the ping in ${Math.floor(newMessageTimestamp - oldMessageTimestamp)} milliseconds!`);
+        }
+    });
+    
+    client.on('message', message => {
+    
+        if (command === `ping`) {
+    
+            message.channel.send('🏓 Pinging...').then(message => {
+    
+                message.edit('Pong!\nPonged back the ping in milliseconds!');
+            })
+        }
+    });
 }
