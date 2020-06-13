9e2e8ba9026f7b2759dbfb882bcb1f6e634a8bc5
Americhino <rileyengo@gmail.com>
2020-06-13 12:00:13 -0600

    oops

--- commands/kick.js
+++ commands/kick.js
@@ -1,6 +1,6 @@
 exports.run = (Discord, client, message, [mention, reason]) => {
 	client.on("message", (message) => {
-		if (message.content.startsWith("/kick")) {
+		if (message.content.startsWith("!kick")) {
 			// Easy way to get member object though mentions.
 			var member= message.mentions.members.first();
 			// Kick
