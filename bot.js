f644d27f8f1b4fa4595c64525efa5bf7d015f848
americhino <rileyengo@gmail.com>
2020-10-17 14:33:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require("./music.js");
-const games = ["Ranma 1/2", "At the dojo", "With P-chan", "Don't make me wild like you!", "我爱你! Wo ai ni!", "Romeo and Juliet",];
-client.on('ready', () => { /*
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (games.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5). 
-        */
-	client.user.setPresence({ activity: { name: 'with P-chan' /* games[index] */ }, status: 'idle' }); // sets bot's activities to one of the phrases in the arraylist.
-	/*
-	}, 10000); // Runs this every 10 seconds.
-	*/
+const games = ["Ranma 1/2", "At the dojo", "With P-chan", "Don't make me wild like you!", "我爱你! Wo ai ni!", "Romeo and Juliet",]; 
+// client.on('ready', () => { 
+//    setInterval(() => {
+//      const index = Math.floor(Math.random() * (games.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5). 
+//		client.user.setPresence({ activity: { name: 'with P-chan' /* games[index] */ }, status: 'idle' }); // sets bot's activities to one of the phrases in the arraylist.
+//	}, 10000); // Runs this every 10 seconds.
+// });
+client.on('ready', () => { 
+//	client.user.setPresence({ activity: { name: 'with P-chan' /* games[index] */ }, status: 'idle' }); // sets bot's activities to one of the phrases in the arraylist.
+	client.user.setActivity(`with P-chan`);
 });
 client.on('message', message => {
 	const prefix = '!';
