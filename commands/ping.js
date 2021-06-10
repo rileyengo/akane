5f293ff0133607cdfaf623c8454cad15742d07b2
americhino <rileyengo@gmail.com>
2021-06-10 08:09:18 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,4 @@
 exports.run = (Discord, client, message, args) => {
-    const ping = m.createdTimestamp - message.createdTimestamp
-    message.channel.send("🏓 Pong!").then((sentMessage) => sentMessage.edit("🏓 Pong! `" + ping + "ms`"))
+    message.channel.send("🏓 Pong!").then((sentMessage) => sentMessage.edit("🏓 Pong! `" + client.ping + "ms`"))
 }
 
