05ffdff69b3f70e3990a39bd5735477f29bf9b26
americhino <rileyengo@gmail.com>
2021-06-09 19:33:14 -0600

    rearrange asia, fix bahrain capital

--- commands/trivia/questions.js
+++ commands/trivia/questions.js
@@ -1,6 +1,6 @@
 /* @submodule		Akane/Questions
  * @description		Collection of questions for Akane's !trivia command.
- * @todo			Make it switch questions every time the user does !trivia
+ * @todo		Make it switch questions every time the user does !trivia
 */
 module.exports.Questions = "World Capitals Trivia by Americhino"
 	module.exports.QText   = QSelect(); // The questions
@@ -8,16 +8,6 @@ module.exports.Questions = "World Capitals Trivia by Americhino"
 function QSelect() {
 	module.exports.Questions = new Array("Norway", "Hungary", "Cyprus", "Libya", "Kenya", "Nigeria", "Columbia", "Barbados", "Australia", "Pakistan", "Spain", "United Kingdom", "Ireland", "Sweden", "Denmark", "Poland", "Italy", "India", "Canada", "Portugal", "South Korea", "Japan", "China", "Indonesia", "France", "Serbia", "Russia", "Ukraine", "United States", "Thailand", "Philippines", "Afghanistan", "Armenia", "Azerbaijan", "Bahrain", "Bangladesh", "Bhutan", "Brunei", "Cambodia", "Georgia", "Iran", "Iraq", "Jordan", "Kazakhstan", "Kuwait", "Kyrgyzstan", "Laos", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Myanmar", "Nepal", "North Korea", "Oman", "Papua New Guinea", "Qatar", "Saudi Arabia", "Singapore", "Sri Lanka", "Syria", "Taiwan", "Tajikistan", "Turkey", "Turkmenistan", "United Arab Emirates (UAE)", "Uzbekistan", "Vietnam", "Yemen",);
 	module.exports.Answers = {
-	"Norway": "Oslo",
-	"Hungary": "Budapest",
-	"Cyprus": "Nicosia",
-	"Libya": "Tripoli",
-	"Kenya": "Nairobi",
-	"Nigeria": "Lagos",
-	"Columbia": "Bogota",
-	"Barbados": "Bridgetown",
-	"Australia": "Canberra",
-	"Pakistan": "Islamabad",
 	"Spain": "Madrid", // No cheating!
 	"United Kingdom" : "London",
 	"Ireland": "Dublin",
@@ -25,32 +15,31 @@ function QSelect() {
 	"Denmark": "Copenhagen",
 	"Poland": "Warsaw",
 	"Italy": "Rome",
-	"India": "New Delhi",
 	"Canada": "Ottawa",
 	"Portugal": "Lisbon",
-	"South Korea": "Seoul",
-	"Japan": "Tokyo",
-	"China": "Beijing",
-	"Indonesia": "Jakarta",
 	"France": "Paris",
 	"Serbia": "Belgrade",
 	"Russia": "Moscow",
 	"Ukraine": "Kiev",
 	"United States": "Washington, D.C.",
-	"Thailand": "Bangkok",
-	"Philippines": "Manila",
+	// Asia
 	"Afghanistan": "Kabul",
 	"Armenia": "Yerevan",
 	"Azerbaijan": "Baku",
+	"Bahrain": "Manama",
 	"Bangladesh": "Dhaka",
 	"Bhutan": "Thimpu",
 	"Brunei": "Bandar Seri Begawan",
 	"Cambodia": "Phnom Penh",
+	"China": "Beijing",
 	"East Timor": "Dili",
 	"Georgia": "Tbilisi",
+	"India": "New Delhi",
+	"Indonesia": "Jakarta",
 	"Iran": "Tehran",
 	"Iraq": "Baghdad",
 	"Israel": "Jerusalem",
+	"Japan": "Tokyo",
 	"Jordan": "Amman",
 	"Kazakhstan": "Nur-Sultan",
 	"Kuwait": "Kuwait City",
@@ -62,21 +51,35 @@ function QSelect() {
 	"Mongolia": "Ulaanbataar",
 	"Myanmar": "Naypiydaw",
 	"Nepal": "Kathmandu",
-	"North Korea": "Pyongyang", // ahahhhah pog
+	"North Korea": "Pyongyang",
 	"Oman": "Muscat",
+	"Pakistan": "Islamabad",
 	"Papua New Guinea": "Port Moresby",
+	"Philippines": "Manila",
 	"Qatar": "Doha",
 	"Saudi Arabia": "Riyadh",
 	"Singapore": "Singapore",
-	"Sri Lanka": "Sri Jayawardenepura Kotte", // rip in pieces non-geography nerds and non-sri lankans
+	"South Korea": "Seoul",
+	"Sri Lanka": "Colombo", 
 	"Syria": "Damascus",
 	"Taiwan": "Taipei", // urg don't sue me pro-china people srry
 	"Tajikistan": "Dushanbe",
+	"Thailand": "Bangkok",
 	"Turkey": "Ankara",
 	"Turkmenistan": "Ashgabat",
 	"United Arab Emirates (UAE)": "Abu Dhabi",
 	"Uzbekistan": "Tashkent",
 	"Vietnam": "Hanoi",
 	"Yemen": "Sana'a",
+	//
+	"Norway": "Oslo",
+	"Hungary": "Budapest",
+	"Cyprus": "Nicosia",
+	"Libya": "Tripoli",
+	"Kenya": "Nairobi",
+	"Nigeria": "Lagos",
+	"Columbia": "Bogota",
+	"Barbados": "Bridgetown",
+	"Australia": "Canberra",
 	}
 }
