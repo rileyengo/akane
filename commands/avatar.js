91b196b699fa05d90f555729db4c1e5e71fae747
americhino <rileyengo@gmail.com>
2021-06-10 20:34:39 -0600

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ exports.run = (Discord, client, message, args) => {
 		const embed = new Discord.MessageEmbed()
 			// .setTitle(member.user.tag)
 			// .setImage(member.user.displayAvatarURL()) // I KNOW THIS IS THE CRUCIAL PART JEEZ
-			.setAuthor(member.user.username, message.author.displayAvatarURL())
+			.setAuthor(message.author.username, message.author.displayAvatarURL())
 			.setColor(0x00AE86)
 		message.channel.send({embed});
 }
