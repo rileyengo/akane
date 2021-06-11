a97ab4c33ce2ae6e0f1e2aaa59290b9286b3bf09
americhino <rileyengo@gmail.com>
2021-06-10 19:50:20 -0600

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,9 +1,9 @@
 exports.run = (Discord, command, client, message, args) => {
-		let member = message.mentions.members.first();
+		let member = message.mentions.users.first();
 		const avatar = message.author.displayAvatarURL()
 		const embed = new Discord.MessageEmbed()
 			.setTitle(member.user.tag)
-			.setAuthor(member.user.username, message.author.displayAvatarURL())
+			.setAuthor(member.user.username, member.user.displayAvatarURL())
 			.setColor(0x00AE86)
-			message.channel.send({files: [file], embed: {embed}});
+			message.channel.send({files: [avatar], embed: {embed}});
 }
