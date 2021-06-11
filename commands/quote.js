dee756b18ce4caf68ea5f8309e776df76f4ad3ef
americhino <rileyengo@gmail.com>
2021-06-10 20:43:19 -0600

    Update quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -1,8 +1,8 @@
 exports.run = (Discord, client, message, args) => {
-	const randomQuote = require("./data/quotes.js");
+	const randomQuote = require("./commands/data/quotes.js");
 	const embed = new Discord.MessageEmbed()
 		.setTitle("**Random Quote**")
-  		.setDescription('```js\n\"' + randomQuote.quote + '```')
+  		.setDescription('```js\n\"' + randomQuote.quote + '"```')
 	  	.setColor(5301186)
   		.setTimestamp()
 	  	.setFooter(message.author.tag, message.author.displayAvatarURL())
