2b1c5066788234427f5b0edb36ac98a8c32187e7
americhino <rileyengo@gmail.com>
2021-06-10 19:53:59 -0600

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,5 +5,8 @@ exports.run = (Discord, command, client, message, args) => {
 			.setTitle(member.user.tag)
 			.setAuthor(member.user.username, member.user.displayAvatarURL())
 			.setColor(0x00AE86)
+		if (!message.mentions.users.size) {
+			return message.reply('mention a user pls');
+		}
 			message.channel.send({files: [avatar], embed: {embed}});
 }
