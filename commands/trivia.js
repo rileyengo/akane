d6ed97552fcd431751993e3edcd79f80b20494c4
riley <rileyengo@gmail.com>
2021-09-14 19:17:47 -0600

    aesthetic changes?

--- commands/trivia.js
+++ commands/trivia.js
@@ -3,11 +3,11 @@ exports.run = (Discord, client, message, args) => {
 	const question = questions.Questions[Math.floor(Math.random() * Math.floor(questions.Questions.length))];
 	const answer = questions.Answers[question]
 	const embed = new Discord.MessageEmbed()
-		.setTitle("🗺 World Capitals")
+		.setTitle("🗺 world capitals")
 		.setAuthor(message.author.username, message.author.displayAvatarURL())
 		.setColor(16272203)
 		.setTimestamp()
-		.setDescription('📍 **What is the capital of ' + question + '?** \n You have 15 seconds to answer, **case-sensitive**.')
+		.setDescription('📍 **what is the capital of ' + question + '?** \n you have 15 seconds to answer, **case-sensitive** (capitalized).')
     message.channel.send({embed})
 		.then(() => {
 		message.channel.awaitMessages(response => response.content === answer, {
@@ -17,22 +17,22 @@ exports.run = (Discord, client, message, args) => {
 		})
 		.then((collected) => {
 			const embed = new Discord.MessageEmbed()
-				.setTitle("✅ Correct!")
+				.setTitle("✅ correct!")
 				.setAuthor(message.author.username, message.author.displayAvatarURL())
 				.setColor(6734080)
 				.setTimestamp()
 				.setFooter(message.author.username, message.author.displayAvatarURL())
-				.setDescription('The answer was **' + answer + '**.')
+				.setDescription('the answer was **' + answer + '**.')
 			message.channel.send({embed})
 		})
 		.catch(() => {
 			const embed = new Discord.MessageEmbed()
-				.setTitle("❌ Incorrect!")
+				.setTitle("❌ incorrect!")
 				.setAuthor(message.author.username, message.author.displayAvatarURL())
 				.setColor(13632027)
 				.setTimestamp()
 				.setFooter(message.author.username, message.author.displayAvatarURL())
-				.setDescription('The answer was **' + answer + '**.')
+				.setDescription('the answer was **' + answer + '**.')
 			message.channel.send({embed})
 		});
 	}); 
